dbcb34fe7046c394b12b13f331407f2d4c096dde
Jasper Alakoskela <jasper.alakoskela@gmail.com>
2021-11-08 13:20:14 +0200

    aloitus

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title></title>
+</head>
+<body>
+    <div class="container-fluid container-expand-lg container-dark bg-primary text-center">
+        <h2>Päivämäärälaskuri</h2>
+        <h4>Luo oma lähtölaskenta mihin tahansa päivään</h4>
+    </div>
+    
+    <div class="container">
+
+        <div class="row">
+            <div class="card bg-light col-md-3 " >
+                
+                <div class="card-body">
+                    
+                </div>
+            </div>
+
+            <div class="card bg-light col-md-6" >
+                <div class="card-header text-center"><h3>Lähtölaskenta</h3></div>
+                <div class="card-body">
+                    
+                </div>
+            </div>
+
+            <div class="card bg-light col-md-3 " >
+                
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+        
+
+    
+   
+
+
+
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery.min.js"></script>
+</body>
+</html>
